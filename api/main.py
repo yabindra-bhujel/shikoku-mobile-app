2edd851a6622bbbf622166d9b2b85d66f69de292
yabindra-bhujel <yabindra2057@gmail.com>
2024-05-13 11:31:49 +0900

    [fixed allow origins]

--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI,Depends
 from src.auth import router as auth_router
 from src.auth.permissions import authenticate_user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
+
 app.include_router(auth_router.router)
 
 @app.get("/")
