a5418362b1503f2f7233a60a7b314c4464fe5137
DCNTT <vanthanh050@gmail.com>
2024-06-21 16:48:12 +0900

    re-styled icon-container background-color

--- services/client/src/screens/Home/HomeScreenIcon.tsx
+++ services/client/src/screens/Home/HomeScreenIcon.tsx
@@ -8,52 +8,52 @@ const HomeScreenIcon = () => {
   return (
     <View>
       <View style={styles.iconLineContainer}>
-        <TouchableOpacity style={styles.iconContainer}>
+        <TouchableOpacity style={[styles.iconContainer, styles.redBackground]}>
           <View style={styles.iconItSelt}>
-            <FontAwesome5 name="home" size={40} color="black" />
+            <FontAwesome5 name="home" size={40} color="white" />
           </View>
           <Text style={styles.iconTitle}>Home</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.iconContainer}
+          style={[styles.iconContainer, styles.blueBackground]}
           onPress={() => router.push("/calendar")}
         >
           <View style={styles.iconItSelt}>
-            <FontAwesome5 name="home" size={40} color="black" />
+            <FontAwesome5 name="home" size={40} color="white" />
           </View>
           <Text style={styles.iconTitle}>Calendar</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.iconLineContainer}>
         <TouchableOpacity
-          style={styles.iconContainer}
+          style={[styles.iconContainer, styles.greenBackground]}
           onPress={() => router.push("/chatbot")}
         >
           <View style={styles.iconItSelt}>
-            <FontAwesome5 name="robot" size={40} color="black" />
+            <FontAwesome5 name="robot" size={40} color="white" />
           </View>
           <Text style={styles.iconTitle}>ChatBot</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}>
+        <TouchableOpacity style={[styles.iconContainer, styles.orangeBackground]}>
           <View style={styles.iconItSelt}>
-            <FontAwesome5 name="home" size={40} color="black" />
+            <FontAwesome5 name="home" size={40} color="white" />
           </View>
           <Text style={styles.iconTitle}>Home</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.iconLineContainer}>
-        <TouchableOpacity style={styles.iconContainer}>
+        <TouchableOpacity style={[styles.iconContainer, styles.purpleBackground]}>
           <View style={styles.iconItSelt}>
-            <FontAwesome5 name="home" size={40} color="black" />
+            <FontAwesome5 name="home" size={40} color="white" />
           </View>
           <Text style={styles.iconTitle}>Home</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.iconContainer}
+          style={[styles.iconContainer, styles.yellowBackground]}
           onPress={() => router.push("/setting")}
         >
           <View style={styles.iconItSelt}>
-            <AntDesign name="setting" size={40} color="black" />
+            <AntDesign name="setting" size={40} color="white" />
           </View>
           <Text style={styles.iconTitle}>Settings</Text>
         </TouchableOpacity>
@@ -65,9 +65,6 @@ const HomeScreenIcon = () => {
 export default HomeScreenIcon;
 
 const styles = StyleSheet.create({
-  home: {
-    backgroundColor: "red",
-  },
   iconLineContainer: {
     marginHorizontal: 50,
     marginBottom: 50,
@@ -90,7 +87,7 @@ const styles = StyleSheet.create({
     borderColor: "white",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#F08080",
+    backgroundColor: "#CA7DF9",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -98,21 +95,30 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.3,
     shadowRadius: 4.65,
-
     elevation: 8,
-  },
-  iconItSelt1: {
-    height: 100,
-    width: 100,
-    borderRadius: 20,
-    marginBottom: 5,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "blue",
   },
   iconTitle: {
     fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
+    color: "white"
+  },
+  redBackground: {
+    backgroundColor: "red",
+  },
+  blueBackground: {
+    backgroundColor: "blue",
+  },
+  greenBackground: {
+    backgroundColor: "green",
+  },
+  orangeBackground: {
+    backgroundColor: "orange",
+  },
+  purpleBackground: {
+    backgroundColor: "purple",
+  },
+  yellowBackground: {
+    backgroundColor: "#724cf9",
   },
 });
