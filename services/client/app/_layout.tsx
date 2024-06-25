e2feba72d21999eb3cbc0469789b568943e3a2f1
DCNTT <vanthanh050@gmail.com>
2024-06-25 18:52:58 +0900

    remove theme provider

--- services/client/app/_layout.tsx
+++ services/client/app/_layout.tsx
@@ -1,17 +1,14 @@
 import { Stack } from "expo-router";
-import {ThemeProvider} from "@/src/hooks/ThemeContext";
 
 const RootLayout = () => {
   
   return (
-    <ThemeProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
-    </ThemeProvider>
   );
 };
 
