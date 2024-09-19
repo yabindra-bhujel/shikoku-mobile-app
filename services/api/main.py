a479b9b5b02d44b72897fcdddd80f55171e347a0
yabindra-bhujel <yabindra2057@gmail.com>
2024-09-19 20:09:29 +0900

    Add test api for notification

--- services/api/main.py
+++ services/api/main.py
@@ -121,6 +121,12 @@ async def get():
     return HTMLResponse(html)
 
 
+# test router
+@app.get("/test")
+async def test():
+    return {"message": "Hello World"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
