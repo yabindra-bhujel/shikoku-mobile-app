72d2cba3cb4f78dac3d39fcc225d986feeb77bde
yabindra-bhujel <yabindra2057@gmail.com>
2024-09-19 21:01:22 +0900

    [migration]

--- services/api/src/models/entity/notification_token.py
+++ services/api/src/models/entity/notification_token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, DateTime, ForeignKey, Text
+from sqlalchemy import Column, Integer, DateTime, ForeignKey, Text, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from ..database import Base
@@ -10,9 +10,10 @@ class ExpoToken(Base):
     id = Column(Integer, primary_key=True, index=True)
     token = Column(Text, unique=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-
+    is_active = Column(Boolean, default=True)
     user = relationship('User', back_populates='expo_token')
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
 
     
     def __repr__(self):
