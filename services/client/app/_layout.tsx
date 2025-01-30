0a444a4f5f14f6cc0f36268b5bcec37d04d73839
dcntt <vanthanh050@gmail.com>
2025-01-30 10:02:33 +0900

    add navigationcontainer to new sdk52

--- services/client/app/_layout.tsx
+++ services/client/app/_layout.tsx
@@ -1,14 +1,16 @@
-import { Stack } from "expo-router";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Stack } from 'expo-router';
 
 const RootLayout = () => {
-  
   return (
+    <NavigationContainer>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="modal" options={{ presentation: "modal" }} /> */}
       </Stack>
+    </NavigationContainer>
   );
 };
 
